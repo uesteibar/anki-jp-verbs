da9c7328e035fec4fb52b4561e2cb1273be0d566
uesteibar <uesteibar@gmail.com>
2024-06-17 19:49:14 +0800

    Create decks and notes

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
+import { select, confirm } from '@inquirer/prompts'
 import { VERBS } from "./src/verbs.js"
-import { getDecks } from "./src/anki.js";
-import { NEGATIVE, PAST, PAST_NEGATIVE, NEGATIVE_FORMAL, PAST_FORMAL, PAST_NEGATIVE_FORMAL } from "./src/conjugator.js";
+import { getDecks, createModel, modelExists, createNote, createDeck } from "./src/anki.js";
+import {
+  NEGATIVE,
+  PAST,
+  PAST_NEGATIVE,
+  NEGATIVE_FORMAL,
+  PAST_FORMAL,
+  PAST_NEGATIVE_FORMAL
+} from "./src/conjugator.js";
 
 import { permutate } from "./src/conjugator.js"
 
-const cards = await permutate(VERBS, [
+const FORMS = [
   NEGATIVE,
   PAST,
   PAST_NEGATIVE,
   NEGATIVE_FORMAL,
   PAST_FORMAL,
   PAST_NEGATIVE_FORMAL,
-])
+]
+const cards = await permutate(VERBS, FORMS)
 
-console.log(cards);
+const decks = await getDecks()
 
-console.log(await getDecks());
+const deck = await select({
+  message: "📖 Select the deck to which you want to import verb form flashcards",
+  choices: decks.map(d => ({
+    name: d,
+    value: d,
+  }))
+})
+
+console.log(`\nImporting a total of ${cards.length} flashcards, for the following forms:
+${FORMS.map(f => `\n- ${f.name}`)}
+`)
+
+let shouldContinue = await confirm({ message: `Flashcards will be added as sub-decks, one deck per verb form. Shall I proceed?` });
+if (!shouldContinue) {
+  console.log("\n❌ Alrighty! Cancelling.")
+  process.exit()
+}
+
+const shouldCreateModel = !(await modelExists())
+
+if (shouldCreateModel) {
+  shouldContinue = await confirm({ message: `I will first create a new card model for you. Is that alright?` });
+  if (!shouldContinue) {
+    console.log("\n❌ Alrighty! Cancelling.")
+    process.exit()
+  }
+
+  await createModel()
+} else {
+  console.log("\nModel 'jp-verb-conjugation' already exists. I'll use that.")
+}
+
+const formRunners = FORMS.map(f => {
+  return async () => createDeck(f, deck)
+})
+formRunners.reduce((p, fn) => p.then(fn), Promise.resolve())
+
+await new Promise(r => setTimeout(r, 2000));
+
+const runners = cards.map(card => {
+  return async () => createNote(card, deck)
+})
+
+runners.reduce((p, fn) => p.then(fn), Promise.resolve())
--- src/anki.js
+++ src/anki.js
@@ -5,3 +5,115 @@ export const getDecks = async () => {
 
   return client.deck.deckNames()
 }
+
+export const createModel = async () => {
+  const client = new YankiConnect()
+
+  return client.model.createModel({
+    modelName: "jp-verb-conjugation",
+    inOrderFields: [
+      "dictionaryForm",
+      "tense",
+      "english",
+      "conjugation",
+      "furigana",
+      "verbType"
+    ],
+    css: `
+    .card {
+       font-family: "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "Noto Sans JP", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", "MS UI Gothic", sans-serif;
+       font-size: 56px;
+       text-align: center;
+    }
+
+    img {
+      max-width: 300px;
+      max-height: 250px;
+    }
+
+    .mobile img {
+      max-width: 50vw;
+    }
+
+    /* This part defines the bold color. */
+    b{color: #5586cd}
+
+    .hide {
+      display: none;
+    }
+    `,
+    cardTemplates: [
+      {
+        "Name": "Verb conjugation",
+        "Front": `
+        <div lang="ja">
+          {{dictionaryForm}}
+          <div id="sentence">
+            <hr>
+            <div style='font-size: 20px;'>{{tense}}</div>
+            <hr>
+          </div>
+        </div>
+        `,
+        "Back": `
+        <div lang="ja">
+          <div id="furigana">
+            {{furigana:furigana}}
+          </div>
+
+          <div style='font-size: 14px; padding-bottom:16px;'>
+            {{english}}
+          </div>
+        </div>
+        `
+      }
+    ]
+  })
+}
+
+export const modelExists = async () => {
+  const client = new YankiConnect()
+  const models = await client.model.modelNames()
+
+  return !!models.find(m => m === "jp-verb-conjugation")
+}
+
+export const createDeck = async (form, parent) => {
+  const client = new YankiConnect()
+
+  const name = `${parent}::${form.name}`
+
+  const decks = await getDecks()
+
+  if (!!decks.find(m => m === name)) return
+
+  console.log(`Creating deck ${name}`)
+
+  return client.deck.createDeck({deck: `${parent}::${form.name}`})
+}
+
+export const createNote = async (verb, deck) => {
+  const client = new YankiConnect()
+
+  const deckName = `${deck}::${verb.tense}`
+
+  await client.note.addNote({
+    note: {
+      deckName: deckName,
+      modelName: "jp-verb-conjugation",
+      fields: {
+        dictionaryForm: verb.dictionaryForm,
+        tense: verb.tense,
+        english: verb.english,
+        conjugation: verb.conjugation,
+        furigana: verb.furigana,
+        verbType: verb.verbType,
+      },
+      options: {
+        allowDuplicate: true,
+        duplicateScope: deckName,
+      },
+      tags: ["jp-verb-conjugations"]
+    }
+  })
+}
